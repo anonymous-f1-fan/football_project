4d686b8cf268ed4217ac1b124fc72a6980e62dc5
anonymous-f1-fan <105464153+anonymous-f1-fan@users.noreply.github.com>
2022-06-12 23:14:40 +0300

    Add files via upload

--- pages/Page_4._FIFA.py
+++ pages/Page_4._FIFA.py
@@ -134,7 +134,7 @@ with st.echo(code_location='below'):
     st.pyplot(fig)
 
     """Как мы видим, разность между потенциалом и текущими способностями действительно падает с возрастом, а к 30 годам достигает 0. 
-    Однако потом регрессия пердстказывает, что раность продолжит уменьшаться и станет отрицательной. На самом деле, как мы увидели ранее, так 
+    Однако потом регрессия предсказывает, что разность продолжит уменьшаться и станет отрицательной. На самом деле, как мы увидели ранее, так 
     и должно быть (футболисты стареют и начинают играть хуже). Но разработчики FIFA считают немного иначе: для старых игроков в качестве потенциального уровня 
     они указывают текущий уровень, а не тот, который был на пике. Поэтому разность между потенциалом и текущим рейтингом не может быть отрицательной в FIFA, хотя 
     в реальности она обычно отрицательная."""
@@ -142,7 +142,7 @@ with st.echo(code_location='below'):
     st.subheader("Анализ зависимости рейтинга от разных параметров")
     st.sidebar.subheader("Анализ зависимости рейтинга от разных параметров")
 
-    """Теперь вернёмся к показатлеям игроков и общим рейтингам. Попробуем понять, как формируются рейтинги игроков."""
+    """Теперь вернёмся к показателям игроков и общим рейтингам. Попробуем понять, как формируются рейтинги игроков."""
 
     """Первое предположение, которое кажется логичным: чем больше сумма всех параметров, тем выше общий рейтинг.
     В среднем это действительно так, в чём несложно убедиться построив регрессию."""
@@ -168,7 +168,7 @@ with st.echo(code_location='below'):
 
     """Однако так же понятно и видно, что такая оценка оказывается очень неточной. Поэтому я предлагаю попробовать оценить общий 
     рейтинг не по сумме всех показателей, а по 6 основным показателям. Для этого нужно построить линейную регрессию по 6 переменным. 
-    В этом случае у нас не будет одной линией, которая будет усреднять значение общего рейтинга по переменной, а будет целая область. 
+    В этом случае у нас не будет одной линии, которая будет усреднять значение общего рейтинга по переменной, а будет целая область. 
     Причём можно изобразить эти области в осях общий рейтинг-переменная. Сделав это, получим 6 графиков, где голубым показаны реальные значения 
     для игроков, а розовым - предсказываемые."""
 
@@ -227,9 +227,187 @@ with st.echo(code_location='below'):
     st.pyplot(fig)
 
     """Однако даже из этих картинок не до конца понятно, каким будет рейтинг при заданных параметрах. Но для того, чтобы 
-    разобраться с этим я (напишу) телеграм-бота, который будет предказывать рейтинг футболиста с заданными параметрами."""
+    разобраться с этим я написал телеграм-бота, который предсказывает рейтинг футболиста с заданными параметрами. В следующем подразделе будет описание 
+    этого бота и инструкция, как им пользоваться. А сейчас лишь скажу, что такая предсказательная модель оказалась тоже не очень точной. Это произошло из-за того, 
+    что мы рассматривали всех полевых игроков, не учитывая, на какой позийии они играют. Понятное дело, что в реальной жизни защитные и атакующие качества для защитников и 
+    нападающих имеют разную ценность."""
+
+    """Поэтому я построил другую предсказательную модель, где рассматриваются те же параметры, но берутся лишь футболисты, которые являются центральными нападающими. 
+    Ниже вы можете увидеть, какие предсказания делает эта модель."""
+
+    df['positions'] = df['player_positions'].str.find('ST')
+    df2 = df[lambda x: x['positions'] > -1][
+        ['overall', 'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]
+
+    model = LinearRegression()
+    model.fit(df2[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']], df2['overall'])
+
+    fig, ax = plt.subplots(2, 3, sharey=True, figsize=(12, 8))
+    df0.plot.scatter(x="pace", y="overall", ax=ax[0][0], color='steelblue')
+    ax[0][0].plot(
+        df0["pace"],
+        model.predict(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]),
+        "o",
+        color="salmon",
+        alpha=0.2)
+
+    df0.plot.scatter(x="shooting", y="overall", ax=ax[0][1], color='steelblue')
+    ax[0][1].plot(
+        df0["shooting"],
+        model.predict(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]),
+        "o",
+        color="salmon",
+        alpha=0.2)
+
+    df0.plot.scatter(x="passing", y="overall", ax=ax[0][2], color='steelblue')
+    ax[0][2].plot(
+        df0["passing"],
+        model.predict(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]),
+        "o",
+        color="salmon",
+        alpha=0.2)
+
+    df0.plot.scatter(x="dribbling", y="overall", ax=ax[1][0], color='steelblue')
+    ax[1][0].plot(
+        df0["dribbling"],
+        model.predict(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]),
+        "o",
+        color="salmon",
+        alpha=0.2)
+
+    df0.plot.scatter(x="defending", y="overall", ax=ax[1][1], color='steelblue')
+    ax[1][1].plot(
+        df0["defending"],
+        model.predict(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]),
+        "o",
+        color="salmon",
+        alpha=0.2)
+
+    df0.plot.scatter(x="physic", y="overall", ax=ax[1][2], color='steelblue')
+    ax[1][2].plot(
+        df0["physic"],
+        model.predict(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]),
+        "o",
+        color="salmon",
+        alpha=0.2)
+
+    st.pyplot(fig)
+
+    st.subheader("Телеграм-бот")
+    st.sidebar.subheader("Телеграм-бот")
+
+    """Итак, как я сказал ранее, я также сделал небольшого бота, который позволяет выбрать одну из предсказательных моделей и 
+    применить её к различным футболистам."""
+
+    """Чтобы посмотреть, как работают эти модели на практике, сделайте следующее:"""
+
+    st.markdown("**1.** Найдите в телеграме бота с тегом **@FIFA_predictor_bot**.")
+    st.markdown("**2.** Напишите этому боту **/start**.")
+    st.markdown("**3.** Напишите **/model**, чтобы выбрать одну из моделей.")
+    st.markdown("**4.** Напишите **/model1**, чтобы выбрать первую модель (менее точную, где используются все полевые игроки), или "
+                "**/model2**, чтобы выбрать вторую модель (где рассматриваются только центральные нападающие).")
+    st.markdown("**5.** Напишите фамилию игрока, для которого хотите посмотреть характеристики, его общий рейтинг в FIFA и предсказываемый рейтинг. "
+                "Если Вы не знаете, какого игрока выбрать, то можете выбрать кого-то из списка: "
+                "Cristiano Ronaldo, Messi, Neymar Jr, Chalov, Dzyuba.")
+    st.markdown("**6.** Далее Вы можете посмотреть информацию про другого игрока или выбрать другую модель, написав **/model** "
+                "и действуя по инструкции.")
+
+    """Я предлагаю Вам посмотреть, как работают обе построенные модели для разных игроков и увидеть, что обе из них являются не до конца точными. 
+    Неточность первой модели я уже объяснил, а неточность во второй модели возникакет по следующим причинам. Во-первых, 
+    моя модель - сильное упрощение реальной оценки общего рейтинга футболистов. Скорее всего рейтинг зависит не только от 
+    шести основных параметров, но и от других (возможно, скрытых от нас) показтелей. Кроме того, зависимость от этих показателей, 
+    вероятнее всего, не линейная, а полиномиальная. Из-за этого вторая модель тоже оказывается не до конца точной, но более точной, чем первая."""
 
 
+
+
+
+
+    # Код, который запускает бота (частично взято с https://habr.com/ru/post/442800/)
+    #bot = telebot.TeleBot('5578986612:AAE-GbvQC6Okh5EerbPWaMDMJWXwqVm_nKs')
+
+
+    #def get_data(data):
+    #    return pd.read_csv(data, delimiter=',')
+
+
+    #df = get_data('players_20.csv')[lambda x: x['player_positions'] != 'GK']
+
+    #df['surname'] = df['short_name'].str.split('.').apply(lambda x: x[-1]).str.strip()
+
+    #df0 = df[['surname', 'overall', 'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]
+
+    #model1 = LinearRegression()
+    #model1.fit(df0[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']], df0['overall'])
+
+    #df['positions'] = df['player_positions'].str.find('ST')
+    #df2 = df[lambda x: x['positions'] > -1][
+    #    ['surname', 'overall', 'pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']]
+
+    #model2 = LinearRegression()
+    #model2.fit(df2[['pace', 'shooting', 'passing', 'dribbling', 'defending', 'physic']], df2['overall'])
+
+    #model = LinearRegression()
+
+
+    #@bot.message_handler(content_types=['text'])
+    #def start(message):
+     #   if message.text == '/model':
+     #       bot.send_message(message.from_user.id, "Выберите модель. Для этого напишите /model1 или /model2")
+     #       bot.register_next_step_handler(message, get_model)
+     #   else:
+     #       bot.send_message(message.from_user.id, 'Напишите /model, чтобы выбрать модель')
+
+
+    #def get_model(message):
+    #    global model
+    #    if message.text == '/model1':
+    #        model = model1
+    #        bot.send_message(message.from_user.id, 'Теперь выберите футболиста')
+    #        bot.register_next_step_handler(message, get_player)
+    #    elif message.text == '/model2':
+    #        model = model2
+    #        bot.send_message(message.from_user.id, 'Теперь выберите футболиста')
+    #        bot.register_next_step_handler(message, get_player)
+    #    else:
+    #        bot.send_message(message.from_user.id, 'Кажется, Вы ошиблись. Попробуйте снова!')
+    #        bot.send_message(message.from_user.id, "Выберите модель. Для этого напишите /model1 или /model2")
+    #        bot.register_next_step_handler(message, get_model)
+
+
+    #def get_player(message):
+    #    global a
+    #    if message.text in df0['surname'].unique():
+    #        a = message.text
+    #        df1 = df0[lambda x: x['surname'] == a].reset_index()
+    #        prediction = model.predict(pd.DataFrame([[df1.loc[0, 'pace'], df1.loc[0, 'shooting'], df1.loc[0, 'passing'],
+    #                                                  df1.loc[0, 'dribbling'], df1.loc[0, 'defending'],
+    #                                                  df1.loc[0, 'physic']]]))
+    #        bot.send_message(message.from_user.id, f"Вы выбрали {a} \n"
+    #                                               f"\n"
+    #                                               f"Показатели футболиста: \n"
+    #                                               f"Pace: {df1.loc[0, 'pace']}\n"
+    #                                               f"Shooting: {df1.loc[0, 'shooting']}\n"
+    #                                               f"Passing: {df1.loc[0, 'passing']}\n"
+    #                                               f"Dribbling: {df1.loc[0, 'dribbling']}\n"
+    #                                               f"Defending: {df1.loc[0, 'defending']}\n"
+    #                                               f"Physic: {df1.loc[0, 'physic']}\n"
+    #                                               f"\n"
+    #                                               f"Рейтинг футболиста в FIFA: {df1.loc[0, 'overall']} \n"
+    #                                               f"Предсказываемый рейтинг: {prediction[0]}")
+    #        bot.send_message(message.from_user.id,
+    #                         'Вы можете выбрать другого футболиста, написав его фамилию, или другую модель, написав /model')
+    #        bot.register_next_step_handler(message, get_player)
+
+    #    elif message.text == '/model':
+    #        bot.send_message(message.from_user.id, 'Выберите модель. Для этого напишите /model1 или /model2')
+    #        bot.register_next_step_handler(message, get_model)
+    #    else:
+    #        bot.send_message(message.from_user.id, 'Такого футболиста нет в базе данных. Попробуйте снова!')
+    #        bot.register_next_step_handler(message, get_player)
+
+
+    #bot.polling(none_stop=True, interval=0)
 
 
 
